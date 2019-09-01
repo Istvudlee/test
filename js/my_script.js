ae07d80e04ee3b7978996d82811fdc80e1d94dae
Istvudlee <istvudlee@mail.ru>
2019-09-01 14:52:06 +0300

    new block + file

--- js/my_script.js
+++ js/my_script.js
@@ -1,7 +1,43 @@
 let buttonMenu = document.querySelector('.menu_but');
 let menu = document.querySelectorAll('.menu_ul, .mail, .work_hour');
-buttonMenu.onclick = function() {
-  for(i = 0; i < menu.length; i++ ) { 
-   menu[i].classList.toggle('mobmenu_show')
+buttonMenu.onclick = function () {
+  for (i = 0; i < menu.length; i++) {
+    menu[i].classList.toggle('mobmenu_show')
   }
+}
+let warp_questions = document.querySelector('.warp_questions');
+let block_answer = document.querySelectorAll('.block_answer');
+let blck_question = document.querySelectorAll('.blck_question');
+warp_questions.onclick = function(e) {
+  e.preventDefault();
+  let target = e.target;
+  let parventtarget = target.parentNode;
+  if(target.tagName != 'A' ) return;
+  for(i = 0; i < block_answer.length; i++) {
+    if(parventtarget.parentNode.contains(block_answer[i])) {
+      block_answer[i].classList.toggle('show_quest')
+    }
+  }
+}
+// Табы документы(Дать на проверку)  = переписать под "делегировагие событий"
+
+let toogledoc = document.querySelector('.toogledoc');
+let toogleTagA = toogledoc.querySelectorAll('a');
+let divdoc = document.querySelectorAll('.contentwr');
+toogledoc.onclick = function(e) {
+	let target = e.target; 
+	if(target.classList === 'activetab') return;
+	for (i=0; i < toogledoc.children.length; i++) {
+    if (toogleTagA[i].classList.contains("activetab")) {
+			toogleTagA[i].classList.remove("activetab");
+		}
+	}
+	target.classList.add('activetab');
+  for(i = 0; i < divdoc.length; i++ ) {
+		divdoc[i].classList.remove("show")
+	 if(target.dataset.number === divdoc[i].dataset.number) {
+		divdoc[i].classList.add("show");
+	 }
+	}
+	return false;
 }
---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+/* Здесь проверяется существование переменных */
+if (isset($_POST['name'])) {$name = $_POST['name'];}
+if (isset($_POST['tel'])) {$phone = $_POST['tel'];}
+ 
+/* Сюда впишите свою эл. почту */
+$myaddres  = "licenss@mail.ru"; // кому отправляем 
+ 
+/* А здесь прописывается текст сообщения, \n - перенос строки */
+$mes = "\nТелефон: $phone\nИмя: $name";
+ 
+/* А эта функция как раз занимается отправкой письма на указанный вами email */
+$sub='Заказ c сайта Автолицензия.рф'; //сабж
+$email='автолицензия.рф'; // от кого
+$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
+ 
+ini_set('short_open_tag', 'On');
+header('Refresh: 3; URL=index.html');
+?>
